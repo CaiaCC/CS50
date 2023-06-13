8f9cdfe3ccf3e66733b92761edd65567e6aa5a5b
Caia Chuang <caia@Caias-MacBook-Pro.local>
2023-06-13 10:54:44 -0700

    cs50(wk01): Abstraction functions for mario in C.

--- wk1/mario.c
+++ wk1/mario.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// declares the functions for compiler so that main function can be on the top of the file
+int get_size(void);
+void print_grid(int size);
+
 int main(void)
 {
     // Get size of grid
+    int n = get_size();
+
+    // Print grid of bricks
+    print_grid(n);
+}
+
+int get_size(void)
+{
     int n;
     do
     {
         n = get_int("Size: ");
     }
     while (n < 1);
+    return n;
+}
 
-    // Print grid of bricks
-    for (int i = 0; i < n; i++)
+void print_grid(int size)
+{
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < size; j++)
         {
             printf("#");
         }
