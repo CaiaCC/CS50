8b0bb8cfd3f8cf91adefe5be3af334f674643aa5
Caia Chuang <caiachuang@gmail.com>
2023-06-21 16:21:56 -0700

    cs50(wk2): string in C.

--- wk2/hi.c
+++ wk2/hi.c
@@ -6,9 +6,12 @@ int main(void)
     // char c1 = 'H';
     // char c2 = 'i';
     // char c3 = '!';
-    string s = "Hi!";
-    string t = "Bye!";
+    // string s = "Hi!";
+    // string t = "Bye!";
+    string words[2];
+    words[0] = "Hi!";
+    words[1] = "Bye!";
 
-    printf("%s\n", s);
-    printf("%s\n", t);
+    printf("%s\n", words[0]);
+    printf("%s\n", words[1]);
 }
