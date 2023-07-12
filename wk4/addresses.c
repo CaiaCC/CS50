1bcf64217fb7a62292afb74267b3d03144465a4e
Caia Chuang <caiachuang@gmail.com>
2023-07-12 13:11:47 -0700

    cs50(wk4): string is *char.

--- wk4/addresses.c
+++ wk4/addresses.c
@@ -1,10 +1,13 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    int n = 50;
-    int *p = &n; // * => pointer called p, instructs the compiler to go to a location in memory
-    printf("%p\n", p); // & provides the address of sth stored in memory
+    // int n = 50;
+    // int *p = &n; // * => pointer called p, instructs the compiler to go to a location in memory
+    // printf("%p\n", p); // & provides the address of sth stored in memory
+    string s = "HI!"; // char *s, in cs50: typedef char *string;
+    printf("%s\n", s);
 }
 
 // pointer: 8 bytes
