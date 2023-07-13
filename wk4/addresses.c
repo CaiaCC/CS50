99832413cea8621c4fe2ec9d3113924756a556ad
Caia Chuang <caiachuang@gmail.com>
2023-07-13 13:55:45 -0700

    cs50(wk4): copy string.

--- wk4/addresses.c
+++ wk4/addresses.c
@@ -1,13 +1,19 @@
-#include <cs50.h>
+// #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    // int n = 50;
-    // int *p = &n; // * => pointer called p, instructs the compiler to go to a location in memory
-    // printf("%p\n", p); // & provides the address of sth stored in memory
-    string s = "HI!"; // char *s, in cs50: typedef char *string;
+    /*
+    int n = 50;
+    int *p = &n; // * => pointer called p, instructs the compiler to go to a location in memory
+    printf("%i\n", *p); // & provides the address of sth stored in memory
+    */
+    char *s = "HI!"; // char *s, in cs50: typedef char *string;
     printf("%s\n", s);
+    printf("%p\n", &s[0]); // line 12 and 13 are printing the same thing
+    printf("%s\n", s);
+    printf("%s\n", (s+1));
+
 }
 
 // pointer: 8 bytes
