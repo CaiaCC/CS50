96f6a3e6b47f99a3da0589e5f23e16fc49df0c3e
Caia Chuang <caiachuang@gmail.com>
2023-07-13 13:58:56 -0700

    cs50(wk4): refactor copy string.

--- wk4/copy.c
+++ wk4/copy.c
@@ -10,7 +10,7 @@ int main(void)
 
     char *t = malloc(strlen(s) + 1);
 
-    for (int i = 0; i < strlen(s) + 1; i++)
+    for (int i = 0, n = strlen(s) + 1; i < n; i++) // avoid calling func every time
     {
         t[i] = s[i];
     }
