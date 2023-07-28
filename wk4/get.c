ded9c2116909ffa51b46c1bd60d0f82f11e8ac5d
Caia Chuang <caiachuang@gmail.com>
2023-07-28 12:48:52 -0700

    cs50(wk4): memory section.

---
+++ wk4/get.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int
+    /*
+    int x;
+    printf("x: ");
+    scanf("%i", &x);
+    printf("x: %i\n", x);
+    */
+    // string
+    char *s = NULL;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
+}
